03e3dfc142074cf4169801eb4752a049edf0ada3
saravanakumar812 <119047058+saravanakumar812@users.noreply.github.com>
2023-10-25 15:30:57 +0530

    first commit

---
+++ lib/data/cart_items.dart
@@ -0,0 +1,4 @@
+import 'package:bloc1/features/home/models/home_product_data_model.dart';
+//import 'package:flutter_bloc_tutorial/features/home/models/home_product_data_model.dart';
+
+List<ProductDataModel> cartItems = [];
---
+++ lib/data/grocery_data.dart
@@ -0,0 +1,43 @@
+class GroceryData {
+  static List<Map<String, dynamic>> groceryProducts = [
+    {
+      'id': "1",
+      'name': 'Apples',
+      'description': 'Fresh and juicy apples',
+      'price': 2.99,
+      'imageUrl':
+          'https://img.freepik.com/free-photo/apples-red-fresh-mellow-juicy-perfect-whole-white-desk_179666-271.jpg',
+    },
+    {
+      'id': "2",
+      'name': 'Bananas',
+      'description': 'Ripe and delicious bananas',
+      'price': 1.99,
+      'imageUrl':
+          'https://m.media-amazon.com/images/I/51ebZJ+DR4L._AC_UF1000,1000_QL80_.jpg',
+    },
+    {
+      'id': "3",
+      'name': 'Carrots',
+      'description': 'Organic carrots for a healthy diet',
+      'price': 1.49,
+      'imageUrl':
+          'https://img.freepik.com/premium-photo/fresh-carrots-isolated-white_272787-150.jpg',
+    },
+    {
+      'id': "4",
+      'name': 'Milk',
+      'description': 'Fresh dairy milk from local farms',
+      'price': 2.49,
+      'imageUrl':
+          'https://cdn.britannica.com/77/200377-050-4326767F/milk-splashing-glass.jpg',
+    },
+    {
+      'id': "5",
+      'name': 'Bread',
+      'description': 'Homemade whole wheat bread',
+      'price': 3.29,
+      'imageUrl': "https://thumbs.dreamstime.com/b/bread-cut-14027607.jpg",
+    },
+  ];
+}
